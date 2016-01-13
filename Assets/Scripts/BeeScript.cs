f670056e6eb944e925c017b747a6cd38621dfb5d
GhettoVulcan <btolchefske@gmail.com>
2016-01-12 21:50:38 -0600

    Initial Project Creation Hooray!

---
+++ Assets/Scripts/BeeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeScript : MonoBehaviour {
+
+    private Rigidbody2D body;
+    private bool temp = true;
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        if (temp)
+        {
+            body.isKinematic = false;
+            body.AddForce(new Vector2(600f, 600f));
+            temp = false;
+        }
+	}
+}
---
+++ Assets/Scripts/CombScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombScript : MonoBehaviour {
+
+    private SpriteRenderer sr;
+
+    public Sprite emptyComb;
+    public Sprite fullComb;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = fullComb;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseDown()
+    {
+        if(Input.GetKey("mouse 0"))
+        {
+            sr.sprite = emptyComb;
+            //Send the point to the game manager
+        }
+        
+    }
+}
